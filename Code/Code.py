a55f62f57bdaffafc9c0923be55d125f40d75c7f
Sam-Rasing <s.e.rasing@protonmail.com>
2025-10-02 16:51:01 +0200

    Task A improved

--- Code/Code.py
+++ Code/Code.py
@@ -18,15 +18,13 @@ df_SA = pd.read_excel('Data/AG_LND_SUST_PRXCSS.xlsx', index_col=6)
 
 #%%
 
-# find most recent year 
-for i in df_SA.columns[-5:]:
-    if i in df_RI.columns[-6:]:
-        recent_year = i
-    else: 
-        break
+recent_year = min(df_SA.columns[-1], df_RI.columns[-1])
 
-df_SA = df_SA[[recent_year, 'GeoAreaCode']]
-df_RI = df_RI[[recent_year, 'GeoAreaCode']]
+if (df_SA == recent_year).any and (df_RI == recent_year).any:
+    print('most recent year=', recent_year)
+
+df_SA = df_SA[['GeoAreaCode', recent_year]]
+df_RI = df_RI[['GeoAreaCode', recent_year]]
 
 #%% Sanity check
 print('SANITY CHECKS:\n\n')
@@ -45,6 +43,12 @@ print('index:\n\n', df_SA.index,
       '\n\nno missing values:\n\n', pd.notna(df_SA).all(),
       '\n\nmissing values:\n\n', pd.isna(df_SA).sum())
 
+#np.argsort(df_RI.index) == np.argsort(df_SA.index)
+
+
+
+
+#%%
 check = df_SA.index.isin(df_RI.index)
 print('country list is identifal:', np.unique(check) == True)
 
